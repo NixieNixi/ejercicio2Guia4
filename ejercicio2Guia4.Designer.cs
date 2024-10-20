373c01ec7ac77030fccd53dd2d8e28a489f77be0
Nixie <mgmbautista@gmail.com>
2024-10-19 21:27:38 -0600

    Se agrego btnSalir, ciclo for y if

--- ejercicio2Guia4.Designer.cs
+++ ejercicio2Guia4.Designer.cs
@@ -30,19 +30,44 @@ namespace ejercicio2Guia4
         private void InitializeComponent()
         {
             this.btnEjecutar = new System.Windows.Forms.Button();
+            this.lblMultiplos = new System.Windows.Forms.Label();
+            this.btnSalir = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // btnEjecutar
             // 
             this.btnEjecutar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.btnEjecutar.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
             this.btnEjecutar.Font = new System.Drawing.Font("MS Gothic", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnEjecutar.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
-            this.btnEjecutar.Location = new System.Drawing.Point(123, 82);
+            this.btnEjecutar.Location = new System.Drawing.Point(112, 93);
             this.btnEjecutar.Name = "btnEjecutar";
             this.btnEjecutar.Size = new System.Drawing.Size(94, 41);
             this.btnEjecutar.TabIndex = 0;
             this.btnEjecutar.Text = "Ejecutar";
             this.btnEjecutar.UseVisualStyleBackColor = false;
+            this.btnEjecutar.Click += new System.EventHandler(this.btnEjecutar_Click);
+            // 
+            // lblMultiplos
+            // 
+            this.lblMultiplos.AutoSize = true;
+            this.lblMultiplos.Font = new System.Drawing.Font("Ravie", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMultiplos.Location = new System.Drawing.Point(17, 43);
+            this.lblMultiplos.Name = "lblMultiplos";
+            this.lblMultiplos.Size = new System.Drawing.Size(307, 21);
+            this.lblMultiplos.TabIndex = 1;
+            this.lblMultiplos.Text = " Multiplos de 7 de 100 a 700";
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.btnSalir.Location = new System.Drawing.Point(249, 169);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(75, 23);
+            this.btnSalir.TabIndex = 2;
+            this.btnSalir.Text = "Salir";
+            this.btnSalir.UseVisualStyleBackColor = false;
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
             // 
             // ejercicio2Guia4
             // 
@@ -50,16 +75,21 @@ namespace ejercicio2Guia4
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
             this.ClientSize = new System.Drawing.Size(336, 204);
+            this.Controls.Add(this.btnSalir);
+            this.Controls.Add(this.lblMultiplos);
             this.Controls.Add(this.btnEjecutar);
             this.Name = "ejercicio2Guia4";
             this.Text = "ejercicio2Guia4";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button btnEjecutar;
+        private System.Windows.Forms.Label lblMultiplos;
+        private System.Windows.Forms.Button btnSalir;
     }
 }
 
--- ejercicio2Guia4.cs
+++ ejercicio2Guia4.cs
@@ -10,11 +10,45 @@ using System.Windows.Forms;
 
 namespace ejercicio2Guia4
 {
+    /// <summary>
+    /// Autor: NixieNixi
+    /// Fecha: 19/10/2024
+    /// Version: 1.0.0
+    /// Demostracion: Sentencias Iterativas
+    /// Problema: Desarrolle un programa que cuente la cantidad de múltiplos
+    /// de 7 que hay entre 100 y 700.
+    /// </summary>
     public partial class ejercicio2Guia4 : Form
     {
         public ejercicio2Guia4()
         {
             InitializeComponent();
+        }
+
+        private void btnEjecutar_Click(object sender, EventArgs e)
+        {
+            //Declaracion de variables
+            int num = 0, i = 0;
+
+            //Ejecutar el while entre 100 a 700
+
+            // inicializamos num
+            for (num = 100; num <= 700; num++)
+            {
+                if (num % 7 == 0)
+                {
+                    i = i + 1;
+                }
+
+
+            }
+            MessageBox.Show(String.Format("El total de múltiplos de 7 es: {0}", i));
+        }
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            //Pone fin a la ejecucion del Programa
+            Application.Exit();
         }
     }
 }
