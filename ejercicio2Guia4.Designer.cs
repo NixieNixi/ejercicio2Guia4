72440f600a7faef41d7ced739aee5b5789053c14
Nixie <mgmbautista@gmail.com>
2024-10-19 21:13:12 -0600

    Se agrego color sl form y btnEjecutar

---
+++ ejercicio2Guia4.Designer.cs
@@ -0,0 +1,65 @@
+﻿
+namespace ejercicio2Guia4
+{
+    partial class ejercicio2Guia4
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnEjecutar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnEjecutar
+            // 
+            this.btnEjecutar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.btnEjecutar.Font = new System.Drawing.Font("MS Gothic", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEjecutar.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.btnEjecutar.Location = new System.Drawing.Point(123, 82);
+            this.btnEjecutar.Name = "btnEjecutar";
+            this.btnEjecutar.Size = new System.Drawing.Size(94, 41);
+            this.btnEjecutar.TabIndex = 0;
+            this.btnEjecutar.Text = "Ejecutar";
+            this.btnEjecutar.UseVisualStyleBackColor = false;
+            // 
+            // ejercicio2Guia4
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(336, 204);
+            this.Controls.Add(this.btnEjecutar);
+            this.Name = "ejercicio2Guia4";
+            this.Text = "ejercicio2Guia4";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnEjecutar;
+    }
+}
+
--- ejercicio2Guia4.cs
+++ ejercicio2Guia4.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace ejercicio2Guia4
 {
-    public partial class Form1 : Form
+    public partial class ejercicio2Guia4 : Form
     {
-        public Form1()
+        public ejercicio2Guia4()
         {
             InitializeComponent();
         }
